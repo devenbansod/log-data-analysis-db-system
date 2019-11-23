705380a0bb6c9b5003c98cf496a44ce7084021cd
Deven Bansod <devenbansod.bits@gmail.com>
2019-11-23 17:34:57 -0500

    Add ability to watch new files and re-run the algorithms

---
+++ watcher.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+if [[ $(id -u) -ne 0 ]] ; then
+    echo "Need to be root"
+    exit 1
+fi
+
+result=$(git diff | grep "^+" | wc -l)
+echo $result "lines added"
+
+if [ $result > 10 ]
+then
+	echo "Can go ahead"
+fi
+
+git diff | grep "^+" | cut -c2- > data/input.log
+echo "$(tail -n +2 input.log)" > data/input.log
+
+git commit -am "123"
+
+sudo bash upload.sh neo4j deven123 data/input.log
